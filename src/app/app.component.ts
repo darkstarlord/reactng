e07af9744de288c8bd8ae599281897933104fbd0
Dark Starlord <darkstarlord@tuta.io>
2018-04-30 11:07:57 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {HelloComponent} from './react/hello.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  helloComponent: HelloComponent;
+
+  ngOnInit() {
+    this.helloComponent = ReactDOM.render(
+      React.createElement(HelloComponent), 
+      document.getElementById('hello'));
+  }
 }
